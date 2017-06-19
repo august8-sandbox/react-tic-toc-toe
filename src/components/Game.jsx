8a414abdf980dd17bbca74b655364bb2c71b45cc
August8 <hazuki@people.co.jp>
2017-06-19 17:16:29 +0900

    Add history機能の追加

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,16 +1,76 @@
 import React from 'react';
 import Board from './Board';
+import calculateWinner from '../util/CalculateWinner';
 
 export default class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(i) {
+    // const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ? false : true,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    // historyの追加
+    const moves = history.map((step, move) => {
+      const desc = move ? `Move # ${move}` : 'Game start';
+      return (
+        <li key={move}>
+          <a href="##" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+    // 手番の表示
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${(this.state.xIsNext ? 'X' : 'O')}`;
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            handleClick={this.handleClick}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div className="status">{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
